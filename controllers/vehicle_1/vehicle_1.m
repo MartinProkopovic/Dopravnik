221e5ca5105bde30b69fbdc6a6d9e0e8554ea7bd
MiroslavLauko <74052070+MiroslavLauko@users.noreply.github.com>
2020-12-02 00:15:04 +0100

    Update vehicle_1.m

--- controllers/vehicle_1/vehicle_1.m
+++ controllers/vehicle_1/vehicle_1.m
@@ -18,8 +18,8 @@ e_right = wb_robot_get_device('engine_right');
 e_left = wb_robot_get_device('engine_left');
 dist_s = wb_robot_get_device('dist_sens');
 dVs = wb_robot_get_device('dV');
-wb_motor_set_velocity(e_right, 0);
-wb_motor_set_velocity(e_left, 0);
+wb_motor_set_velocity(e_right, 5);
+wb_motor_set_velocity(e_left, 5);
 wb_distance_sensor_enable(dist_s, TIME_STEP);
 wb_distance_sensor_enable(dVs, TIME_STEP);
 while wb_robot_step(TIME_STEP) ~= -1
@@ -28,6 +28,7 @@ dist = wb_distance_sensor_get_value(dist_s);
 vz = wb_distance_sensor_get_value(dVs);
 if vz < 128
 go=1;
+r=0;
 end
 
 if go==1 & r == 0
@@ -35,10 +36,11 @@ if go==1 & r == 0
  
   wb_motor_set_position(e_right, o);
   wb_motor_set_position(e_left, o);
-stop(0.3, TIME_STEP);
+stop(0.01, TIME_STEP);
 
   o= o-1;
-    if dist < 51
+  dist = wb_distance_sensor_get_value(dist_s);
+    if dist < 60
   r = 1;
   end
   
@@ -46,10 +48,11 @@ stop(0.3, TIME_STEP);
   
 
 
-if dist < 51 & r == 1
-
+if dist < 60 & r == 1
 wb_motor_set_position(e_right, 0);
 wb_motor_set_position(e_left, 0); 
+o=-1;
+g=0;
 end
 
   drawnow;
