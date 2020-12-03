c3b92f6c8d6927ee7e7e78428caae73f6b5ecbd9
MartinProkopovic <MartinProkopovic@users.noreply.github.com>
2020-12-03 11:02:20 +0100

    Update vehicle_1.m

--- controllers/vehicle_1/vehicle_1.m
+++ controllers/vehicle_1/vehicle_1.m
@@ -18,10 +18,12 @@ e_right = wb_robot_get_device('engine_right');
 e_left = wb_robot_get_device('engine_left');
 dist_s = wb_robot_get_device('dist_sens');
 dVs = wb_robot_get_device('dV');
+
 wb_motor_set_velocity(e_right, 5);
 wb_motor_set_velocity(e_left, 5);
 wb_distance_sensor_enable(dist_s, TIME_STEP);
 wb_distance_sensor_enable(dVs, TIME_STEP);
+
 while wb_robot_step(TIME_STEP) ~= -1
 
 dist = wb_distance_sensor_get_value(dist_s);
@@ -52,7 +54,7 @@ if dist < 60 & r == 1
 wb_motor_set_position(e_right, 0);
 wb_motor_set_position(e_left, 0); 
 o=-1;
-g=0;
+go=0;
 end
 
   drawnow;
