1b56477df0feb9b0759b6eb721fd989fbcd260df
MiroslavLauko <74052070+MiroslavLauko@users.noreply.github.com>
2020-12-07 00:22:58 +0100

    Update vehicle.m

--- controllers/vehicle/vehicle.m
+++ controllers/vehicle/vehicle.m
@@ -1,14 +1,3 @@
-% MATLAB controller for Webots
-% File:          vehicle.m
-% Date:
-% Description:
-% Author:
-% Modifications:
-
-% uncomment the next two lines if you want to use
-% MATLAB's desktop to interact with the controller:
-%desktop;
-%keyboard;
 
 TIME_STEP = 64;
 p=-1;
@@ -18,20 +7,10 @@ motor_l = wb_robot_get_device('motor_left');
 motor_r = wb_robot_get_device('motor_right');
  c = wb_robot_get_device('dsc');
  Dvs = wb_robot_get_device('Dv');
-% get and enable devices, e.g.:
-%  camera = wb_robot_get_device('camera');
-%  wb_camera_enable(camera, TIME_STEP);
-%  motor = wb_robot_get_device('motor');
- 
-
   wb_distance_sensor_enable(c,TIME_STEP);
   wb_distance_sensor_enable(Dvs,TIME_STEP);
 wb_motor_set_velocity(motor_l,6);
   wb_motor_set_velocity(motor_r,6);
-% main loop:
-% perform simulation steps of TIME_STEP milliseconds
-% and leave the loop when Webots signals the termination
-%
 while wb_robot_step(TIME_STEP) ~= -1
 D=wb_distance_sensor_get_value(c);
 E=wb_distance_sensor_get_value(Dvs);
@@ -79,4 +58,3 @@ function step1(t)
 
   end
   end
-% cleanup code goes here: write data to files, etc.
