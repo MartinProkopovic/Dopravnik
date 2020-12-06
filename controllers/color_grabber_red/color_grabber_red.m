e768a90aec024601f7f3cf87612a365aefd110ab
MiroslavLauko <74052070+MiroslavLauko@users.noreply.github.com>
2020-12-07 00:23:04 +0100

    Update color_grabber_red.m

--- controllers/color_grabber_red/color_grabber_red.m
+++ controllers/color_grabber_red/color_grabber_red.m
@@ -1,15 +1,3 @@
-% MATLAB controller for Webots
-% File:          color_grabber_red.m
-% Date:
-% Description:
-% Author:
-% Modifications:
-
-% uncomment the next two lines if you want to use
-% MATLAB's desktop to interact with the controller:
-%desktop;
-%keyboard;
-
 TIME_STEP = 64;
 m = 0;
 TW = wb_robot_get_device('tw');
@@ -19,7 +7,6 @@ finger_3  = wb_robot_get_device('grabber finger C');
 pivot_1 = wb_robot_get_device('pivot1');
 pivot_2 = wb_robot_get_device('pivot2');
 D_R = wb_robot_get_device('red');
-
 speed = 2;
  wb_motor_set_velocity(TW,speed);
  wb_motor_set_velocity(finger_1,speed);
@@ -31,28 +18,26 @@ speed = 2;
  
 while wb_robot_step(TIME_STEP) ~= -1
 Sens = wb_distance_sensor_get_value(D_R);
-
-
 wb_motor_set_position(TW,1.57)
 pause(0.5,TIME_STEP)
 wb_motor_set_position(finger_1,1.3);
 wb_motor_set_position(finger_2,1.3);
 wb_motor_set_position(finger_3,1.3);
-
 wb_motor_set_position(pivot_2,1);
 wb_motor_set_position(pivot_1,-0.5);
-
+pause(0.5,TIME_STEP)
 Sens = wb_distance_sensor_get_value(D_R);
-if 500 > Sens && Sens > 300
+if 600 > Sens && Sens > 400
 m = 1;
 end
 if m == 1
-pause(1.25,TIME_STEP)
+pause(0.75,TIME_STEP)
 wb_motor_set_position(pivot_2, 1.3);
 wb_motor_set_position(pivot_1, 0.1);
-wb_motor_set_position(finger_1,0);
-wb_motor_set_position(finger_2,0);
-wb_motor_set_position(finger_3,0);
+pause(0.2,TIME_STEP)
+wb_motor_set_position(finger_1,0.2);
+wb_motor_set_position(finger_2,0.2);
+wb_motor_set_position(finger_3,0.2);
 pause(1,TIME_STEP)
 wb_motor_set_position(pivot_2,-0.3);
 wb_motor_set_position(pivot_1,-0.3);
@@ -67,14 +52,8 @@ wb_motor_set_position(finger_3,1.3);
 pause(1,TIME_STEP)
 m = 0;
 end
-
-
-
   drawnow;
-
 end
-
-
 function pause(time_s,wait) 
  start_time = wb_robot_get_time();
   while (start_time + time_s > wb_robot_get_time())
