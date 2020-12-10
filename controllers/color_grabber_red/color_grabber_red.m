59af9ad63ee370cc67f61770a5c9cde6f6f2346d
MiroslavLauko <74052070+MiroslavLauko@users.noreply.github.com>
2020-12-10 21:38:40 +0100

    Update color_grabber_red.m

--- controllers/color_grabber_red/color_grabber_red.m
+++ controllers/color_grabber_red/color_grabber_red.m
@@ -7,7 +7,7 @@ finger_3  = wb_robot_get_device('grabber finger C');
 pivot_1 = wb_robot_get_device('pivot1');
 pivot_2 = wb_robot_get_device('pivot2');
 D_R = wb_robot_get_device('red');
-speed = 2;
+speed = 3;
  wb_motor_set_velocity(TW,speed);
  wb_motor_set_velocity(finger_1,speed);
  wb_motor_set_velocity(finger_2,speed);
@@ -17,9 +17,8 @@ speed = 2;
  wb_distance_sensor_enable(D_R,TIME_STEP);
  
 while wb_robot_step(TIME_STEP) ~= -1
-Sens = wb_distance_sensor_get_value(D_R);
+if m==0
 wb_motor_set_position(TW,1.57)
-pause(0.5,TIME_STEP)
 wb_motor_set_position(finger_1,1.3);
 wb_motor_set_position(finger_2,1.3);
 wb_motor_set_position(finger_3,1.3);
@@ -27,11 +26,15 @@ wb_motor_set_position(pivot_2,1);
 wb_motor_set_position(pivot_1,-0.5);
 pause(0.5,TIME_STEP)
 Sens = wb_distance_sensor_get_value(D_R);
-if 1000 > Sens && Sens > 400
+end
+while wb_robot_step(m==0)
+Sens = wb_distance_sensor_get_value(D_R);
+if 1000 > Sens && Sens > 450
 m = 1;
 end
+end
 if m == 1
-pause(0.75,TIME_STEP)
+pause(0.85,TIME_STEP)
 wb_motor_set_position(pivot_2, 1.3);
 wb_motor_set_position(pivot_1, 0.1);
 pause(0.2,TIME_STEP)
@@ -43,7 +46,7 @@ wb_motor_set_position(pivot_2,-0.3);
 wb_motor_set_position(pivot_1,-0.3);
 pause(1,TIME_STEP)
 wb_motor_set_position(TW,-1.57);
-pause(2,TIME_STEP)
+pause(1,TIME_STEP)
 wb_motor_set_position(pivot_2,0.3);
 pause(1,TIME_STEP)
 wb_motor_set_position(finger_1,1.3);
@@ -62,6 +65,5 @@ function pause(time_s,wait)
 end
 function step(t) 
   if (wb_robot_step(t) == -1) 
-    wb_robot_cleanup();
   end
 end
