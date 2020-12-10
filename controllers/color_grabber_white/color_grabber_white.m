92a80a46221aba26c524dcdf67933709c63247eb
MiroslavLauko <74052070+MiroslavLauko@users.noreply.github.com>
2020-12-10 21:38:44 +0100

    Update color_grabber_white.m

--- controllers/color_grabber_white/color_grabber_white.m
+++ controllers/color_grabber_white/color_grabber_white.m
@@ -7,7 +7,7 @@ finger_3  = wb_robot_get_device('grabber finger C');
 p_1 = wb_robot_get_device('p1');
 p_2 = wb_robot_get_device('p2');
 D_W = wb_robot_get_device('white');
-speed = 2;
+speed = 3;
  wb_motor_set_velocity(W,speed);
  wb_motor_set_velocity(finger_1,speed);
  wb_motor_set_velocity(finger_2,speed);
@@ -17,9 +17,8 @@ speed = 2;
  wb_distance_sensor_enable(D_W,TIME_STEP);
  
 while wb_robot_step(TIME_STEP) ~= -1
-COL = wb_distance_sensor_get_value(D_W);
+if z==0
 wb_motor_set_position(W,-1.57)
-pause(0.5,TIME_STEP)
 wb_motor_set_position(finger_1,1.3);
 wb_motor_set_position(finger_2,1.3);
 wb_motor_set_position(finger_3,1.3);
@@ -27,11 +26,15 @@ wb_motor_set_position(p_2,1);
 wb_motor_set_position(p_1,-0.5);
 pause(0.5,TIME_STEP)
 COL = wb_distance_sensor_get_value(D_W);
+end 
+while wb_robot_step(z==0)
+COL = wb_distance_sensor_get_value(D_W);
 if 250 > COL && COL > 100
 z = 1;
 end
+end
 if z == 1
-pause(0.75,TIME_STEP)
+pause(0.85,TIME_STEP)
 wb_motor_set_position(p_2, 1.3);
 wb_motor_set_position(p_1, 0.1);
 pause(0.2,TIME_STEP)
@@ -43,7 +46,7 @@ wb_motor_set_position(p_2,-0.3);
 wb_motor_set_position(p_1,-0.3);
 pause(1,TIME_STEP)
 wb_motor_set_position(W,1.57);
-pause(2,TIME_STEP)
+pause(1,TIME_STEP)
 wb_motor_set_position(p_2,0.3);
 pause(1,TIME_STEP)
 wb_motor_set_position(finger_1,1.3);
@@ -51,6 +54,7 @@ wb_motor_set_position(finger_2,1.3);
 wb_motor_set_position(finger_3,1.3);
 pause(1,TIME_STEP)
 z = 0;
+
 end
   drawnow;
 end
@@ -62,6 +66,5 @@ function pause(time_s,wait)
 end
 function step(t) 
   if (wb_robot_step(t) == -1) 
-    wb_robot_cleanup();
   end
 end
