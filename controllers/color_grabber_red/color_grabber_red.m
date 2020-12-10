a006bdcb7ffd6c17f5f41f02879e1d0360686262
MiroslavLauko <74052070+MiroslavLauko@users.noreply.github.com>
2020-12-10 07:00:36 +0100

    Update color_grabber_red.m

--- controllers/color_grabber_red/color_grabber_red.m
+++ controllers/color_grabber_red/color_grabber_red.m
@@ -1,37 +1,51 @@
 TIME_STEP = 64;
 m = 0;
 TW = wb_robot_get_device('tw');
+TW2 = wb_robot_get_device('twister');
 finger_1 = wb_robot_get_device('grabber finger A');
 finger_2  = wb_robot_get_device('grabber finger B');
 finger_3  = wb_robot_get_device('grabber finger C');
 pivot_1 = wb_robot_get_device('pivot1');
 pivot_2 = wb_robot_get_device('pivot2');
+pivot_3 = wb_robot_get_device('pivot3');
 D_R = wb_robot_get_device('red');
 speed = 2;
  wb_motor_set_velocity(TW,speed);
+  wb_motor_set_velocity(TW2,speed);
  wb_motor_set_velocity(finger_1,speed);
  wb_motor_set_velocity(finger_2,speed);
  wb_motor_set_velocity(finger_3,speed);
  wb_motor_set_velocity(pivot_1,speed);
  wb_motor_set_velocity(pivot_2,speed);
+  wb_motor_set_velocity(pivot_3,speed);
  wb_distance_sensor_enable(D_R,TIME_STEP);
  
 while wb_robot_step(TIME_STEP) ~= -1
-Sens = wb_distance_sensor_get_value(D_R);
+if m==0
 wb_motor_set_position(TW,1.57)
 pause(0.5,TIME_STEP)
-wb_motor_set_position(finger_1,1.3);
-wb_motor_set_position(finger_2,1.3);
-wb_motor_set_position(finger_3,1.3);
-wb_motor_set_position(pivot_2,1);
-wb_motor_set_position(pivot_1,-0.5);
-pause(0.5,TIME_STEP)
+
+wb_motor_set_position(pivot_3,1.57);
+wb_motor_set_position(TW2,-1.57);
+pause(1,TIME_STEP)
+
+wb_motor_set_position(pivot_1,-0.1);
+pause(1,TIME_STEP)
+
 Sens = wb_distance_sensor_get_value(D_R);
 if 1000 > Sens && Sens > 400
 m = 1;
 end
+end
+disp(m)
 if m == 1
-pause(0.75,TIME_STEP)
+pause(0.1,TIME_STEP)
+wb_motor_set_position(pivot_2,1.57);
+pause(0.3,TIME_STEP)
+wb_motor_set_position(finger_1,1);
+wb_motor_set_position(finger_2,1);
+wb_motor_set_position(finger_3,1);
+pause(0.2,TIME_STEP)
 wb_motor_set_position(pivot_2, 1.3);
 wb_motor_set_position(pivot_1, 0.1);
 pause(0.2,TIME_STEP)
@@ -39,17 +53,21 @@ wb_motor_set_position(finger_1,0.2);
 wb_motor_set_position(finger_2,0.2);
 wb_motor_set_position(finger_3,0.2);
 pause(1,TIME_STEP)
-wb_motor_set_position(pivot_2,-0.3);
 wb_motor_set_position(pivot_1,-0.3);
+wb_motor_set_position(pivot_2,-0.3);
 pause(1,TIME_STEP)
 wb_motor_set_position(TW,-1.57);
 pause(2,TIME_STEP)
+wb_motor_set_position(TW2,-1.57);
 wb_motor_set_position(pivot_2,0.3);
 pause(1,TIME_STEP)
 wb_motor_set_position(finger_1,1.3);
 wb_motor_set_position(finger_2,1.3);
 wb_motor_set_position(finger_3,1.3);
 pause(1,TIME_STEP)
+wb_motor_set_position(finger_1,0);
+wb_motor_set_position(finger_2,0);
+wb_motor_set_position(finger_3,0);
 m = 0;
 end
   drawnow;
