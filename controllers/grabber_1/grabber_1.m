70d9700c05cedf680e4deeb4f000cb25142ab150
MiroslavLauko <74052070+MiroslavLauko@users.noreply.github.com>
2020-12-16 16:03:16 +0100

    Update grabber_1.m

--- controllers/grabber_1/grabber_1.m
+++ controllers/grabber_1/grabber_1.m
@@ -7,7 +7,7 @@ TIME_STEP = 64;
   finger_c  = wb_robot_get_device('grabber finger C');
   DS_L  = wb_robot_get_device('DS_L');
   DS_R  = wb_robot_get_device('DS_R');
- ref  = wb_robot_get_device('ref');
+  ref  = wb_robot_get_device('ref');
   ref2  = wb_robot_get_device('ref2');
   speed = 3;
   wb_motor_set_velocity(twister,speed);
@@ -18,8 +18,8 @@ TIME_STEP = 64;
   wb_motor_set_velocity(finger_c,speed);
   wb_distance_sensor_enable(DS_L,TIME_STEP);
   wb_distance_sensor_enable(DS_R,TIME_STEP);
-    wb_distance_sensor_enable(ref,TIME_STEP);
-      wb_distance_sensor_enable(ref2,TIME_STEP);
+  wb_distance_sensor_enable(ref,TIME_STEP);
+  wb_distance_sensor_enable(ref2,TIME_STEP);
 
 while wb_robot_step(TIME_STEP) ~= -1
 Ds=wb_distance_sensor_get_value(DS_R);
@@ -27,120 +27,73 @@ dS=wb_distance_sensor_get_value(DS_L);
 r=wb_distance_sensor_get_value(ref);
 r2=wb_distance_sensor_get_value(ref2);
 if Ds < 45 || r<900
-pause(2.0,TIME_STEP);
+wb_robot_step(2000);
 wb_motor_set_position(twister,1.57);
-pause(1.0,TIME_STEP);
+wb_robot_step(1000);
 wb_motor_set_position(finger_a,1.1);
 wb_motor_set_position(finger_b,1.1);
 wb_motor_set_position(finger_c,1.1);
-pause(0.5,TIME_STEP);
+wb_robot_step(500);
 wb_motor_set_position(pivot_B,1.2);
 wb_motor_set_position(pivot_A,0.4);
-pause(0.5,TIME_STEP);
-wb_motor_set_position(finger_a,0.9);
-wb_motor_set_position(finger_b,0.9);
-wb_motor_set_position(finger_c,0.9);
-pause(0.1,TIME_STEP);
-wb_motor_set_position(finger_a,0.7);
-wb_motor_set_position(finger_b,0.7);
-wb_motor_set_position(finger_c,0.7);
-pause(0.1,TIME_STEP);
-wb_motor_set_position(finger_a,0.5);
-wb_motor_set_position(finger_b,0.5);
-wb_motor_set_position(finger_c,0.5);
-pause(0.1,TIME_STEP);
-wb_motor_set_position(finger_a,0.3);
-wb_motor_set_position(finger_b,0.3);
-wb_motor_set_position(finger_c,0.3);
-pause(0.1,TIME_STEP);
+wb_robot_step(500);
 wb_motor_set_position(finger_a,0);
 wb_motor_set_position(finger_b,0);
 wb_motor_set_position(finger_c,0);
-pause(1.0,TIME_STEP);
+wb_robot_step(1000);
 wb_motor_set_position(pivot_A,0);
 wb_motor_set_position(pivot_B,0);
-pause(0.5,TIME_STEP);
 wb_motor_set_position(twister,0);
-pause(1.0,TIME_STEP);
+wb_robot_step(1000);
 wb_motor_set_position(pivot_A,-0.3);
 wb_motor_set_position(pivot_B,0.7);
-pause(0.5,TIME_STEP);
-wb_motor_set_position(pivot_A,-0.6);
-wb_motor_set_position(pivot_B,1.5);
-pause(1.0,TIME_STEP);
+wb_robot_step(50);
+wb_motor_set_position(pivot_A,-0.4);
+wb_motor_set_position(pivot_B,1.4);
+wb_robot_step(1000);
 wb_motor_set_position(finger_a,1.1);
 wb_motor_set_position(finger_b,1.1);
 wb_motor_set_position(finger_c,1.1);
-pause(1.0,TIME_STEP);
+wb_robot_step(1000);
 wb_motor_set_position(pivot_A,0);
 wb_motor_set_position(pivot_B,0);
-pause(1.0,TIME_STEP);
+wb_robot_step(100);
 end
 
 
 if dS < 70 || r2<900
-pause(2.0,TIME_STEP);
+wb_robot_step(2000);
 wb_motor_set_position(twister,-1.57);
-pause(1.0,TIME_STEP);
+wb_robot_step(1000);
 wb_motor_set_position(finger_a,1.1);
 wb_motor_set_position(finger_b,1.1);
 wb_motor_set_position(finger_c,1.1);
-pause(0.5,TIME_STEP);
+wb_robot_step(500);
 wb_motor_set_position(pivot_B,1.2);
 wb_motor_set_position(pivot_A,0.4);
-pause(0.5,TIME_STEP);
-wb_motor_set_position(finger_a,0.9);
-wb_motor_set_position(finger_b,0.9);
-wb_motor_set_position(finger_c,0.9);
-pause(0.1,TIME_STEP);
-wb_motor_set_position(finger_a,0.7);
-wb_motor_set_position(finger_b,0.7);
-wb_motor_set_position(finger_c,0.7);
-pause(0.1,TIME_STEP);
-wb_motor_set_position(finger_a,0.5);
-wb_motor_set_position(finger_b,0.5);
-wb_motor_set_position(finger_c,0.5);
-pause(0.1,TIME_STEP);
-wb_motor_set_position(finger_a,0.3);
-wb_motor_set_position(finger_b,0.3);
-wb_motor_set_position(finger_c,0.3);
-pause(0.1,TIME_STEP);
+wb_robot_step(500);
 wb_motor_set_position(finger_a,0);
 wb_motor_set_position(finger_b,0);
 wb_motor_set_position(finger_c,0);
-pause(1,TIME_STEP);
+wb_robot_step(1000);
 wb_motor_set_position(pivot_A,0);
 wb_motor_set_position(pivot_B,0);
-pause(0.5,TIME_STEP);
 wb_motor_set_position(twister,0);
-pause(1.0,TIME_STEP);
+wb_robot_step(1000);
 wb_motor_set_position(pivot_A,-0.3);
 wb_motor_set_position(pivot_B,0.7);
-pause(0.5,TIME_STEP);
-wb_motor_set_position(pivot_A,-0.6);
-wb_motor_set_position(pivot_B,1.5);
-pause(0.5,TIME_STEP);
+wb_robot_step(50);
+wb_motor_set_position(pivot_A,-0.4);
+wb_motor_set_position(pivot_B,1.4);
+wb_robot_step(1000);
 wb_motor_set_position(finger_a,1.1);
 wb_motor_set_position(finger_b,1.1);
 wb_motor_set_position(finger_c,1.1);
-pause(1.0,TIME_STEP);
+wb_robot_step(1000);
 wb_motor_set_position(pivot_A,0);
 wb_motor_set_position(pivot_B,0);
-pause(1.0,TIME_STEP);
+wb_robot_step(100);
 end
 
   drawnow;
-end
-
-
-function pause(time_s,wait) 
- start_time = wb_robot_get_time();
-  while (start_time + time_s > wb_robot_get_time())
-    step(wait);
-    end
-end
-function step(t) 
-  if (wb_robot_step(t) == -1) 
-    
-  end
 end
